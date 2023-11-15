7d1daf479145fc76a9b723b2a34c366bf351e70c
danbi <leedanbiii@naver.com>
2023-11-16 03:00:31 +0900

    Querydsl 설정

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,35 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
+@Rollback(value = false)
 class QuerydslApplicationTests {
-
+    @Autowired
+    EntityManager em;
     @Test
     void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+        JPAQueryFactory query = new JPAQueryFactory(em);
+//        QHello qHello = new QHello("h");
+        QHello qHello = QHello.hello;
+        Hello result = query.selectFrom(qHello).fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
     }
 
 }
