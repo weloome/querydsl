30fdefdc1b1f23d70095d1eba4e8d17cbb0e448f
danbi <leedanbiii@naver.com>
2023-11-16 04:36:31 +0900

    결과 조회

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,5 +1,6 @@
 package study.querydsl;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.assertj.core.api.Assertions;
@@ -10,6 +11,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.Team;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static study.querydsl.entity.QMember.*;
@@ -83,5 +86,30 @@ public class QuerydslBasicTest {
                 )
                 .fetchOne();
         assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+    @Test
+    public void resultFetch() {
+        // List
+        List<Member> fetch = queryFactory
+                .selectFrom(member)
+                .fetch();
+        // 단 건
+        Member fetchOne = queryFactory
+                .selectFrom(member)
+                .fetchOne();
+        // 처음 한 건
+        Member fetchFirst = queryFactory
+                .selectFrom(member)
+                .fetchFirst();
+        // 페이징
+        QueryResults<Member> results = queryFactory
+                .selectFrom(member)
+                .fetchResults();
+        results.getTotal();
+        List<Member> content = results.getResults();
+        // count 쿼리로 변경
+        long total = queryFactory
+                .selectFrom(member)
+                .fetchCount();
     }
 }
