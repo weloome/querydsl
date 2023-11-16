747472334e607f9a0642177fa1fe44faaa881e5d
danbi <leedanbiii@naver.com>
2023-11-17 02:55:02 +0900

    SQL function 호출

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -609,4 +609,32 @@ public class QuerydslBasicTest {
                 .where(member.age.gt(18))
                 .execute();
     }
+
+    @Test
+    public void sqlFunction1() {
+        List<String> result = queryFactory
+                .select(Expressions.stringTemplate(
+                        "function('replace', {0}, {1}, {2})",
+                        member.username, "member", "M"
+                ))
+                .from(member)
+                .fetch();
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    public void sqlFunction2() {
+        List<String> result = queryFactory
+                .select(member.username)
+                .from(member)
+                .where(member.username.eq(
+//                        Expressions.stringTemplate("function('lower', {0})",member.username)
+                        member.username.lower()
+                )).fetch();
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
 }
